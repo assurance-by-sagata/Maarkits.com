ecf9d1e149aa8e89968e1400d2b35569b7f7d5d8
Arman Jasuja <arman_jasuja@yahoo.com>
2023-11-21 16:39:41 +0400

    Mobile version

---
+++ Maarkits_mobile/app/src/main/java/com/example/maarkits/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.maarkits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+
+
+    }
+}
