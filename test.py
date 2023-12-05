0f8f2c7ab742c0e7829cc857c14a59a3aaa6435c
Arman Jasuja <arman_jasuja@yahoo.com>
2023-12-05 11:11:04 +0400

    Dev Branch Added

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ from helpers import lookup
 
 def test_lookup():
     assert(lookup("TSLA")["symbol"] == "TSLA")
+    assert(lookup("TSLA")["name"] == "TSLA")
+    assert(lookup("GOOG")["name"] == "GOOG")
