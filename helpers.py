5be9baf993e40d86a7e239763cd2d414e1af55cc
Pranav Chaudhary <pranav.chaudhary@sagataltd.io>
2023-12-13 21:24:30 +0400

    changed code duplication in apology function

--- helpers.py
+++ helpers.py
@@ -17,8 +17,10 @@ db = con.cursor(cursor_factory=RealDictCursor)
 
 def apology(message, code=400):
     """Render message as an apology to user."""
+    var = escape(message)
+    return render_template("apology.html", top=code, bottom=var), code
 
-    def escape(s):
+def escape(s):
         """
         Escape special characters.
 
@@ -36,31 +38,9 @@ def apology(message, code=400):
         ]:
             s = s.replace(old, new)
         return s
-
-    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 def apology_test(message, code=400):
     """Render message as an apology to user."""
-
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [
-            ("-", "--"),
-            (" ", "-"),
-            ("_", "__"),
-            ("%s", "~q"),
-            ("%", "~p"),
-            ("#", "~h"),
-            ("/", "~s"),
-            ('"', "''"),
-        ]:
-            s = s.replace(old, new)
-        return s
-
     return (message, code)
 
 
